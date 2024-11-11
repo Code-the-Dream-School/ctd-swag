adcc175debe68baec3d123a5baf6613a7e3566e7
Roy Mosby <10133316+royemosby@users.noreply.github.com>
2024-11-11 11:15:55 -0500

    Convert cart to controlled component (#1)
    * update styles
    
    * load inventory using effect, add cart feature
    
    * add cart logger
    
    * remove details button, add to cart handler
    
    * add props for cart feature
    
    * fix typos
    
    * style cart
    
    * add cart feature
    
    * create cart
    
    * add cart button and counter
    
    * unquote all prices to prevent coersion
    
    * implement a compact cart list
    
    * add .cartScreen
    
    * pass cart state + updater to Cart
    
    * implement cart editing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import './assets/css-reset.css';
 import catalog from './assets/catalog.json';
+import Cart from './Cart';
 import Header from './Header';
 import ProductList from './ProductList';
-import ProductCard from './ProductCard';
 
 function App() {
-  const [inventory, setInventory] = useState(catalog.products);
+  const [inventory, setInventory] = useState([]);
   const [cart, setCart] = useState([]);
+  const [isCartOpen, setIsCartOpen] = useState(false);
   const year = useRef(
     (() => {
       const now = new Date(Date.now());
@@ -16,19 +17,58 @@ function App() {
     })()
   );
 
-  function promoteProduct() {
-    return (
-      <ProductCard
-        name="Special Limited Edition Tee!"
-        description="Special limited edition neon green shirt with a metallic Code the Dream Logo shinier than the latest front-end framework! Signed by the legendary Frank!"
-      />
-    );
+  useEffect(() => {
+    setInventory([...catalog.products]);
+  }, []);
+
+  function handleAddItemToCart(id) {
+    const inventoryItem = inventory.find((item) => item.id === id);
+    if (!inventoryItem) {
+      console.error('cart error: item not found');
+      return;
+    }
+    const itemToUpdate = cart.find((item) => item.id === id);
+    let updatedCartItem;
+    if (itemToUpdate) {
+      updatedCartItem = {
+        ...itemToUpdate,
+        itemCount: itemToUpdate.itemCount + 1,
+      };
+    } else {
+      updatedCartItem = { ...inventoryItem, itemCount: 1 };
+    }
+    setCart([...cart.filter((item) => item.id !== id), updatedCartItem]);
   }
+
+  function handleCloseCart() {
+    //prevents re-render if unchanged
+    if (isCartOpen) {
+      setIsCartOpen(false);
+    }
+  }
+
+  function handleOpenCart() {
+    //prevents re-render if unchanged
+    if (!isCartOpen) {
+      setIsCartOpen(true);
+    }
+  }
+
   return (
     <>
-      <Header cart={cart} />
+      <Header cart={cart} handleOpenCart={handleOpenCart} />
       <main>
-        <ProductList inventory={inventory}>{promoteProduct()}</ProductList>
+        <ProductList
+          inventory={inventory}
+          handleAddItemToCart={handleAddItemToCart}
+        ></ProductList>
+        {isCartOpen && (
+          <Cart
+            cart={cart}
+            setCart={setCart}
+            handleCloseCart={handleCloseCart}
+          />
+        )}
       </main>
       <footer>
         <p>
